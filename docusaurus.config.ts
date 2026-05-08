d0e3cf6d172d712ab4455727fa799e93b0da0db8
Zhengshou Lai <lai.zhengshou@outlook.com>
2026-05-08 22:45:15 +0800

    chore: clean up deploy-related comments in docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,12 +20,9 @@ const config: Config = {
   // Set the production url of your site here
   url: 'https://lzhshou.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
   trailingSlash: true,
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'lzhshou', // Usually your GitHub org/user name.
   projectName: 'lzhshou.github.io', // Usually your repo name.
 
